15dfd774b214093da760b5a757d7988bad4ef05d
Interaction GitLab CI/CD & ArgoCD <deploybot@kuna.family>
2023-02-07 15:59:55 +0100

    deployments and daemonset for test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cluster-resources-scaler
 on off all services types in cluster for cost saving
+
+
+
+    Scaling k8s daemonset down to zero
+
+kubectl -n kube-system patch daemonset myDaemonset -p '{"spec": {"template": {"spec": {"nodeSelector": {"non-existing": "true"}}}}}'
+
+    Scaling up k8s daemonset
+
+kubectl -n kube-system patch daemonset myDaemonset --type json -p='[{"op": "remove", "path": "/spec/template/spec/nodeSelector/non-existing"}]'
+
---
+++ processor.py
@@ -0,0 +1,30 @@
+#!python3
+import command
+
+direction="down"
+
+namespaces=command.run(["kubectl","get","namespaces","-o","jsonpath={.items[*].metadata.name}"]).output.decode('utf-8').split(" ")
+print(namespaces)
+
+exclusionlist=['argocd', 'cert-manager', 'default', 'experimental', 'istio-ingress', 'istio-system', 'kube-node-lease', 'kube-public', 'kube-system', 'kuna-experimental', 'observability', 'socket-cluster',  'test-helm', 'test-helm2', 'vault', 'vault-webhook']
+
+#Deployments scale
+for name in namespaces:
+        if name not in exclusionlist:
+                try:
+                        print("Scaling down all the deployments in",name)
+                        print("-" * 80)
+                        command.run(["kubectl","scale","deployment","-n",name,"--replicas","0","--all"])
+                except Exception as e:
+                        print("Skipping due to error",e)
+                
+# Daemonsets scale
+for name in namespaces:
+
+        if name not in exclusionlist:
+                try:
+                        print("Scaling down all deamonset", name)
+                        print("-" * 80)
+                        command.run(["kubectl", "patch", "myDaemonset", "-n", name, "-p", "'{"spec": {"template": {"spec": {"nodeSelector": {"non-existing": "true"}}}}}'"])
+                except Exception as e:
+                        print("Skipping due to error", e)
