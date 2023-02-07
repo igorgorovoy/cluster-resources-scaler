5d7b1ce0b0e8c97e962f16428769594f396061e8
Interaction GitLab CI/CD & ArgoCD <deploybot@kuna.family>
2023-02-07 18:00:04 +0100

    add up/down logic

--- README.md
+++ README.md
@@ -11,3 +11,44 @@ kubectl -n kube-system patch daemonset myDaemonset -p '{"spec": {"template": {"s
 
 kubectl -n kube-system patch daemonset myDaemonset --type json -p='[{"op": "remove", "path": "/spec/template/spec/nodeSelector/non-existing"}]'
 
+
+```python
+def scale_deployments(dep_name, direction):
+    if direction == "down":
+        scale = 0
+    elif direction == "up":
+        scale = 1
+    print("Direction towards ", scale)
+    try:
+        print("Scaling ", direction ," all the deployments in", dep_name)
+        print("-" * 80)
+        command.run(["kubectl", "scale", "deployment", "-n", dep_name, "--replicas", scale, "--all"])
+    except Exception as e:
+        print("Skipping due to error", e)
+```
+
+```python
+def scale_daemonsets(daemon_name, direction):
+    if direction == "down":
+        try:
+
+            print("Scaling ", direction ,"  deamonset  ",daemon_name," in ", name)
+            print("-" * 180)
+            command.run(["kubectl", "patch", "daemonset", daemon_name , "-n", name, "-p",
+                         "{\"spec\": {\"template\": {\"spec\": {\"nodeSelector\": {\"non-existing\": \"true\"}}}}}"])
+
+        except Exception as e:
+            print("Skipping due to error in scale_daemonsets DOWN", e)
+    elif direction == "up":
+        try:
+
+            print("Scaling ", direction ," deamonset ",daemon_name," in ", name)
+            print("-" * 180)
+            command.run(["kubectl", "patch", "daemonset", daemon_name, "-n", name, "-p",
+                         "[{\"op\": \"remove\", \"path\": \"/spec/template/spec/nodeSelector/non-existing\"}]"])
+
+        except Exception as e:
+            print("Skipping due to error scale_daemonsets UP", e)
+
+
+```
--- processor.py
+++ processor.py
@@ -3,44 +3,6 @@ import command
 
 
 direction = "up" 
-            
-# def scale_deployments(dep_name, direction):
-#     if direction == "down":
-#         scale = 0
-#     elif direction == "up":
-#         scale = 1
-#     print("Direction towards ", scale)
-#     try:
-#         print("Scaling ", direction ," all the deployments in", dep_name)
-#         print("-" * 80)
-#         command.run(["kubectl", "scale", "deployment", "-n", dep_name, "--replicas", scale, "--all"])
-#     except Exception as e:
-#         print("Skipping due to error", e)
-
-
-# def scale_daemonsets(daemon_name, direction):
-#     if direction == "down":
-#         try:
-# 
-#             print("Scaling ", direction ,"  deamonset  ",daemon_name," in ", name)
-#             print("-" * 180)
-#             command.run(["kubectl", "patch", "daemonset", daemon_name , "-n", name, "-p",
-#                          "{\"spec\": {\"template\": {\"spec\": {\"nodeSelector\": {\"non-existing\": \"true\"}}}}}"])
-# 
-#         except Exception as e:
-#             print("Skipping due to error in scale_daemonsets DOWN", e)
-#     elif direction == "up":
-#         try:
-# 
-#             print("Scaling ", direction ," deamonset ",daemon_name," in ", name)
-#             print("-" * 180)
-#             command.run(["kubectl", "patch", "daemonset", daemon_name, "-n", name, "-p",
-#                          "[{\"op\": \"remove\", \"path\": \"/spec/template/spec/nodeSelector/non-existing\"}]"])
-# 
-#         except Exception as e:
-#             print("Skipping due to error scale_daemonsets UP", e)
-
-
 
 namespaces = command.run(["kubectl", "get", "namespaces", "-o", "jsonpath={.items[*].metadata.name}"]).output.decode(
     'utf-8').split(" ")
